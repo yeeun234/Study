154de2ce61e70a5fd374df55f329b8e5bcfdf1d6
yeeun234 <wooyeeun2@gmail.com>
2025-04-05 20:12:48 +0900

    forth try
    1.str.isEmpty() might cause nullpointException since str might be null .
    
    so I should null check before isEmpty.
    
    2.can make it more clear by using || operator.
    
    3.I need to trim the str before checking whether it's null cause sometimes the user might input a spaces.
    
    String input = "     ";
    
    at that point , input.isEmpty() return false (because it technically has characters-spaces)
    
    But logically we want to treat spaces as empty.
    
    if all characters in this string are space (as defined above), then a String object representing an empty string is returned. since this is the manual of Trim(), so it return's empty string and it trigger isempty method.

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -1,77 +1,37 @@
 public class Palindrome {
-
 	public static void main(String[] args) {
 
-
-
 		//What do I want my program to do ?  - checking if a string is a palindrome. 
-
 		String str = "Madam";
-
 		
-
 		boolean isPalindrome = Palindrome.isPalindrome(str);
-
 		if(isPalindrome) {
-
 			System.out.println("yes, "+str+" is a palindrome.");
-
 		}else {
-
-			System.out.println("No, "+str+" is no a palindrome.");
-
+			System.out.println("No, "+str+" is not a palindrome.");
 		}
-
 		
-
 	}
-
 	
-        public static boolean isPalindrome(String str) {
-
-            
-
-            //what if it's null or empty string?
-
-            if(str.isEmpty()) {
-
-                return false;
-
-            }
-
-            if(str==null) {
-
-                return false;
-
-            }
-
-            else{
-
-                //what if it has a spaces and mixed-case?
-
-                //Since Java String is immutable, it always return a new string . so I need to store it in a new variable.
-
-                String cleaned = str.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-
-                
-
-                //reverse it. using reverse method of StringBuilder. and convert it into String for store it into String object.
-
-                String reversed = new StringBuilder(cleaned).reverse().toString();
-
-                
-
-                //compare it with the original string.
-
-                return reversed.equals(cleaned);
-
-                
-
-            }
-
-            
-
-        }
-
-    }
-	
+	public static boolean isPalindrome(String str) {
+		
+		//what if it's null or empty string?
+		if(str == null|| str.trim().isEmpty() ) {
+			return false;
+		}
+		
+		else{
+			//what if it has a spaces and mixed-case?
+			//Since Java String is immutable, it always return a new string . so I need to store it in a new variable.
+			String cleaned = str.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+			
+			//reverse it. using reverse method of StringBuilder. and convert it into String for store it into String object.
+			String reversed = new StringBuilder(cleaned).reverse().toString();
+			
+			//compare it with the original string.
+			return reversed.equals(cleaned);
+			
+		}
+		
+	}
+}
